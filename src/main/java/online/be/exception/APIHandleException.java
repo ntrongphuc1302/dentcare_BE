4cf546cb64dcf72860e59f6c64d23c65df1b07ba
ntrongphuc1302 <ntrongphuc.1302@gmail.com>
2024-06-03 15:43:42 +0700

    fix login

--- src/main/java/online/be/exception/APIHandleException.java
+++ src/main/java/online/be/exception/APIHandleException.java
@@ -13,7 +13,7 @@ public class APIHandleException {
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Object> handleInvalidUsernamePassword(BadCredentialsException ex) {
-        return new ResponseEntity<>("UserName or password not correct!!!", HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>("Username or password not correct!!!", HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
