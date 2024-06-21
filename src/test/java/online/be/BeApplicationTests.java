3f489b6a49be62c926bca54b8929effad05e5316
ntrongphuc1302 <ntrongphuc.1302@gmail.com>
2024-06-22 00:47:29 +0700

    Adding files

--- src/test/java/online/be/BeApplicationTests.java
+++ src/test/java/online/be/BeApplicationTests.java
@@ -1,7 +1,11 @@
 package online.be;
 
+import online.be.entity.Account;
+import online.be.model.request.RegisterRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class BeApplicationTests {
